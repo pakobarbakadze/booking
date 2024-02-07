4dadc9b1eb952de6db7458710823ac810b5c73cd
pakobarbakadze <pakobarbakadze@gmail.com>
2024-02-07 19:10:54 +0400

    feat: reservation module

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,17 @@
+import { DatabaseModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { Reservation, ReservationSchema } from './entities/reservation.entity';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: Reservation.name, schema: ReservationSchema },
+    ]),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService],
+})
+export class ReservationsModule {}
---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,17 @@
+import { AbstractRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Reservation } from './entities/reservation.entity';
+
+@Injectable()
+export class ReservationsRepository extends AbstractRepository<Reservation> {
+  protected readonly logger = new Logger(ReservationsRepository.name);
+
+  constructor(
+    @InjectModel(Reservation.name)
+    reservationModel: Model<Reservation>,
+  ) {
+    super(reservationModel);
+  }
+}
