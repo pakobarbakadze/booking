41246bfc00c96ec069d8c59dcdd379e6dbd49564
pakobarbakadze <pakobarbakadze@gmail.com>
2024-02-07 19:19:25 +0400

    feat: logger

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { ReservationsModule } from './reservations.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,7 +1,8 @@
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { Reservation, ReservationSchema } from './entities/reservation.entity';
 import { ReservationsController } from './reservations.controller';
+import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
 
 @Module({
@@ -10,8 +11,9 @@ import { ReservationsService } from './reservations.service';
     DatabaseModule.forFeature([
       { name: Reservation.name, schema: ReservationSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
